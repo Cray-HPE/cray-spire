40e49c87a7dbf943b09a08ecde661face915c48e
Kevin Burns <77063492+kburns-hpe@users.noreply.github.com>
2023-04-28 11:21:23 -0500

    CASMPET-6488 (#73)
    * CASMPET-6488 Fix kuttl
    
    * CASMPET-6488 Fix dvs-mqtt workloads
    
    * CASMPET-6488 Fix missing value error

--- charts/spire/tests/kuttl/load-kind.sh
+++ charts/spire/tests/kuttl/load-kind.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 #
 # MIT License
 #
-# (C) Copyright 2022-2023 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2023 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -21,8 +22,18 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 #
-kind: Cluster
-apiVersion: kind.x-k8s.io/v1alpha4
-nodes:
-  - role: control-plane
-    image: kindest/node:v1.21.12@sha256:05eefdedfcb6113402ac631782adfa3d9f8b75c38eac783e3da4f44f6404dae0
+
+grep image Chart.yaml | awk '{print $2}' | while read -r image; do
+  docker pull "$image"
+  kind load docker-image "$image"
+done
+
+for image in artifactory.algol60.net/csm-docker/stable/postgres-operator:3.0.0 \
+  artifactory.algol60.net/csm-docker/stable/docker.io/wrouesnel/postgres_exporter:0.8.2 \
+  artifactory.algol60.net/csm-docker/stable/registry.opensource.zalan.do/acid/spilo-12:1.6-p3 \
+  artifactory.algol60.net/csm-docker/stable/registry.opensource.zalan.do/acid/spilo-14:2.1-p7 \
+  artifactory.algol60.net/csm-docker/stable/registry.opensource.zalan.do/acid/postgres-operator:v1.8.2 \
+  artifactory.algol60.net/csm-docker/stable/docker-kubectl:1.19.15; do
+  docker pull "$image"
+  kind load docker-image "$image"
+done
